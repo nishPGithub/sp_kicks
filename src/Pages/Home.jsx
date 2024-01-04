2b6c4de219cc6e3ff280dada1bacedbe6706b1e6
NPATE170 <nispat203@gmail.com>
2024-01-04 10:04:17 +0000

    setting up react app

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from "../Components/Header";
 
 export default function Home() {
     return(
-        <Header />
+        <div className="headerWrapper">
+            <Header />
+        </div>
     )
 }
