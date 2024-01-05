cf357d12c3ebcbfff6e5510a7dcdbc6e12a28b17
NPATE170 <nispat203@gmail.com>
2024-01-05 12:11:40 +0000

    editing header with links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import AppRoutes from "./Components/Routing/AppRoutes";
 import "./Global/global"
+import "./Styles/destyle.css"
 
 function App() {
     const routes = [{ path: "/", element: <Home /> }];
