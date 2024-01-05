fa40a1b9d698d7a61f47fdbb19779263f861141a
NPATE170 <nispat203@gmail.com>
2024-01-05 12:50:24 +0000

    creating sliding transition for tablet menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../../Images/logo.png";
 import Navigation from "../Routing/Navigation";
 
+
+
 export default function Header() {
     const Links = global.model.navigation;
+
+    useEffect(() => {
+        const parent = document.querySelector(".App");
+        const hamburgerMenu = parent.querySelector(".hamburger-menu");
+        const headerNav = parent.querySelector(".headerNav");
+
+        hamburgerMenu.addEventListener("click", () => {
+            console.log("Menu clicked");
+            headerNav.classList.toggle("active");
+        });
+    }, []);
+
     return (
         <header>
             <div className="header">
@@ -17,13 +31,16 @@ export default function Header() {
                                 {Links.map((item, index) => (
                                     <Navigation
                                         index={index}
-                                        class="link"
+                                        class="navLink"
                                         to={item.to}
                                         label={item.label}
                                     />
                                 ))}
                             </ul>
                         </nav>
+                    </div>
+                    <div className="headerNavHamburgerMenu">
+                        <span className="hamburger-menu  material-symbols-outlined">menu</span>
                     </div>
                 </div>
             </div>
